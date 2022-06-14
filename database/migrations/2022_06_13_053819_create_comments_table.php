1af7f76631eba030bfd24e42355a95acb27bd201
jdestin <destin.jord@gmail.com>
2022-06-14 21:43:10 +0200

    test

--- database/migrations/2022_06_13_053819_create_comments_table.php
+++ database/migrations/2022_06_13_053819_create_comments_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
                 $table->id();
                 $table->text('body');
                // $table->nestedSet();
+               
                 $table->foreignId('user_id')
                       ->constrained()
                       ->onDelete('cascade')
